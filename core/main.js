01a5433d2e4c2dccf3178f43107c0b23cd5fa9da
Alan MacLeod <amacleod@rskorbital.com>
2017-05-17 13:28:10 +0700

    Cleanup and document code

--- core/main.js
+++ core/main.js
@@ -1,7 +1,6 @@
 
 import WasmLoader from './WasmLoader';
 
-const INT32_SIZE_IN_BYTES = 4;
 const SCR_WIDTH = 640, SCR_HEIGHT = 480;
 const PAGE_SIZE_BYTES = SCR_WIDTH * SCR_HEIGHT * INT32_SIZE_IN_BYTES;
 
@@ -9,65 +8,38 @@ let w = new WasmLoader();
 
 w.load("./wasm/test").then((wasm) => {
 
-
+  // Setup Canvas and initialise to fill blue
   let c = createCanvas(SCR_WIDTH, SCR_HEIGHT);
   let ctx = c.getContext('2d');
-
   ctx.fillStyle = "#0000ff";
   ctx.fillRect(0,0,SCR_WIDTH, SCR_HEIGHT);
-
+  // Get a reference to the image data bytes `canvasData.data`
   let canvasData = ctx.getImageData(0, 0, SCR_WIDTH, SCR_HEIGHT);
 
-  // let testArray = new Uint8ClampedArray(PAGE_SIZE_BYTES);
-  // testArray.fill(0);
-  // canvasData.data.set(testArray)
-
+  // Allocate a buffer on the heap for our WASM code to write into
   let HEAP_buffer_ptr8 = wasm._malloc(PAGE_SIZE_BYTES);
-  //let view = new DataView(wasm.buffer, HEAP_buffer_ptr8, PAGE_SIZE_BYTES);
+
+  // Now create a reverse-reference to the WASM heap as a JS TypedArray
+  // Which we can manipulate easily JS-side, e.g. copy into the Canvas
   let view = new Uint8ClampedArray(wasm.buffer, HEAP_buffer_ptr8, PAGE_SIZE_BYTES);
 
-  wasm._addOne(128, HEAP_buffer_ptr8, PAGE_SIZE_BYTES);
 
   let time = performance.now();
 
   for (let t=0; t<60; t++)
   {
+    // Call the WASM/C code!
+    wasm._addOne(128, HEAP_buffer_ptr8, PAGE_SIZE_BYTES);
+
+    // Write it's output into our Canvas buffer
     canvasData.data.set(view);
+
+    // Display on screen!
     ctx.putImageData(canvasData, 0, 0);
   }
 
-  console.log("60 time = ", performance.now() - time);
+  console.log("60 fps time = ", performance.now() - time);
 
-
-  // Create array of five int32s
-  //let numInt32 = 5;
-  //let testArray = new Int32Array([10,11,12,13,14]);
-  //let doubleBuffer = new Uint8Array(PAGE_SIZE_BYTES);
-
-  // Allocate bytes on the WASM heap (max 4GB) and get *int (as 32-bit JS int)
-  // which is a byte-aligned offset
-  //let ptr = wasm._malloc(PAGE_SIZE_BYTES);
-
-  //let offset32 = ptr / INT32_SIZE_IN_BYTES;
-  //let offset8 = ptr;
-
-  // Copy data from our ArrayBuffer onto the heap via above *int
-  // divide 8-bit byte offset by 4 to get 32-bit offset
-  //wasm.HEAP32.set(testArray, offset32);
-
-  //let view = new DataView(wasm.buffer, ptr, numInt32 * INT32_SIZE_IN_BYTES);
-
-  //console.log(wasm);
-
-  // wasm._addOne(ptr);
-
-  // let startTime = performance.now();
-  // for (let t=0; t<60; t++)
-  // {
-  //   testArray.set(wasm.HEAP32.subarray(offset32, offset32 + numInt32));
-  // }
-
-  // console.log(wasm._memcpy);
 
 });
 
